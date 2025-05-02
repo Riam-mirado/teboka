aa274d385497b3d7164e301c2182fad4b0c4f381
Riam-mirado <rarimirado@gmail.com>
2025-05-02 21:36:28 +0300

    next...

---
+++ src/services/morseService.jsx
@@ -0,0 +1,371 @@
+const morseCodemap = [
+    {
+        alphabet: 'A',
+        value: '.-',
+        correspondance: 'Allô'
+    },
+    {
+        alphabet: 'B',
+        value: '-...',
+        correspondance: 'Bonaparte'
+    },
+    {
+        alphabet: 'C',
+        value: '-.-.',
+        correspondance: 'Coca-cola'
+    },
+    {
+        alphabet: 'D',
+        value: '-..',
+        correspondance: 'Docile'
+    },
+    {
+        alphabet: 'E',
+        value: '.',
+        correspondance: 'et'
+    },{
+        alphabet: 'F',
+        value: '..-.',
+        correspondance: 'Farandole'
+    },
+    {
+        alphabet: 'G',
+        value: '--.',
+        correspondance: 'Gondole'
+    },
+    {
+        alphabet: 'H',
+        value: '....',
+        correspondance: 'Himalaya'
+    },
+    {
+        alphabet: 'I',
+        value: '..',
+        correspondance: 'Ici'
+    },
+    {
+        alphabet: 'J',
+        value: '.---',
+        correspondance: 'Jablonavo'
+    },
+    {
+        alphabet: 'K',
+        value: '-.-',
+        correspondance: 'Kominform'
+    },
+    {
+        alphabet: 'L',
+        value: '.-..',
+        correspondance: 'Limonade'
+    },
+    {
+        alphabet: 'M',
+        value: '--',
+        correspondance: 'Moto'
+    },
+    {
+        alphabet: 'N',
+        value: '-.',
+        correspondance: 'Noël'
+    },
+    {
+        alphabet: 'O',
+        value: '---',
+        correspondance: 'Oporto'
+    },
+    {
+        alphabet: 'P',
+        value: '.--.',
+        correspondance: 'Philosophie'
+    },
+    {
+        alphabet: 'Q',
+        value: '--.-',
+        correspondance: 'Qocorico'
+    },
+    {
+        alphabet: 'R',
+        value: '.-.',
+        correspondance: 'Revolver'
+    },
+    {
+        alphabet: 'S',
+        value: '...',
+        correspondance: 'Samedi'
+    },
+    {
+        alphabet: 'T',
+        value: '-',
+        correspondance: 'Thon'
+    },
+    {
+        alphabet: 'U',
+        value: '..-',
+        correspondance: 'Urinoir'
+    },
+    {
+        alphabet: 'V',
+        value: '...-',
+        correspondance: 'Valentino'
+    },
+    {
+        alphabet: 'W',
+        value: '.--',
+        correspondance: 'Wagon long'
+    },
+    {
+        alphabet: 'X',
+        value: '-..-',
+        correspondance: 'oXIdation'
+    },
+    {
+        alphabet: 'Y',
+        value: '-.--',
+        correspondance: 'Yoga dodo'
+    },
+    {
+        alphabet: 'Z',
+        value: '--..',
+        correspondance: 'Zoroastre'
+    },
+]
+const opposes = ["A-N", "E-T", "I-M", "S-O", "H-CH", "U-D", "V-B", "R-K", "P-X", "L-Y", "F-Q"]
+
+const abreviation = [
+    {
+        abr: "ABT",
+        val: "Environ(about)"
+    },
+    
+    {
+        abr: "AGN",
+        val: "Encore(again)"
+    },
+    {
+        abr: "ANT",
+        val: "Antenne"
+    },
+    {
+        abr: "BCP",
+        val: "Beaucoup"
+    },
+    {
+        abr: "BJR",
+        val: "Bonjour"
+    },
+    {
+        abr: "BSR",
+        val: "Bonsoir"
+    },
+    {
+        abr: "C",
+        val: "oui"
+    },
+    {
+        abr: "CFM",
+        val: "confirmer"
+    },
+    {
+        abr: "CLR",
+        val: "Clair"
+    },
+    {
+        abr: "CUL",
+        val: "à plus tard(seu you later) "
+    },
+    {
+        abr: "DR",
+        val: "cher ou chère(dear)"
+    },
+    {
+        abr: "DPT",
+        val: "Département"
+    },
+    {
+        abr: "FB",
+        val: "Bon travail(fine business)"
+    },
+    {
+        abr: "FER",
+        val: "Pour"
+    },
+    {
+        abr: "FREQ",
+        val: "fréquence"
+    },
+    {
+        abr: "GA",
+        val: "bon après-midi(good afternoon)"
+    },
+    {
+        abr: "GB",
+        val: "A revoir (good bye)"
+    },
+    {
+        abr: "GD",
+        val: "bien/ bonne journée(good/good day)"
+    },
+    {
+        abr: "GE",
+        val: "Bonsoir(good evening)"
+    },
+    {
+        abr: "GL",
+        val: "bonne chance"
+    },
+    {
+        abr: "GM",
+        val: "bonjour, le matin(good morning)"
+    },
+    {
+        abr: "GN",
+        val: "Bonne nuit(good night)"
+    },
+    {
+        abr: "HPE",
+        val: "espérer(hope)"
+    },
+    {
+        abr: "HPECUAGN",
+        val: "espére vous revoir(hope see you again)"
+    },
+    {
+        abr: "HPECUL",
+        val: "Espére vous revoir plus tard(hope see you later)"
+    },
+    {
+        abr: "HW",
+        val: "comment(how)"
+    },
+    {
+        abr: "MCI",
+        val: "merci"
+    },
+    {
+        abr: "MNI",
+        val: "beaucoup(many)"
+    },
+    {
+        abr: "MSG",
+        val: "message"
+    },
+    {
+        abr: "NR",
+        val: "à proximité de, au numéro"
+    },
+    {
+        abr: "OK",
+        val: "d'accord"
+    },
+    {
+        abr: "OM",
+        val: "vieil Homme(old man), désigne un opérateur mâle, même jeune"
+    },
+    {
+        abr: "OP",
+        val: "opérateur"
+    },
+    {
+        abr: "PR",
+        val: "pour"
+    },
+    {
+        abr: "PSE",
+        val: "s'il vous plait(please)"
+    },
+    {
+        abr: "RAS",
+        val: "Rien à signaler"
+    },
+    {
+        abr: "RFI",
+        val: "interférence"
+    },
+    {
+        abr: "RIG",
+        val: "Appareil radio"
+    },
+    {
+        abr: "RPRT",
+        val: "rapport"
+    },
+    {
+        abr: "RST",
+        val: "Rapport de signal"
+    },
+    {
+        abr: "RX",
+        val: "récépteur(convention réseau)"
+    },
+    {
+        abr: "SN",
+        val: "bientôt"
+    },
+    {
+        abr: "STN",
+        val: "station"
+    },
+    {
+        abr: "SVP",
+        val: "s'il vous plait"
+    },
+    {
+        abr:"TEMP",
+        val: "température"
+    },
+    
+    {
+        abr:"TKS",
+        val: "merci"
+    },
+    {
+        abr:"TMP",
+        val: "Temporaire"
+    },
+    {
+        abr:"TNX",
+        val: "merci"
+    },
+    {
+        abr:"TU",
+        val: "merci(thank you"
+    },
+    {
+        abr:"TX",
+        val: "trnasmetteur(convention réseau)"
+    },
+    {
+        abr:"UR",
+        val: "ton/ vôtre(your)"
+    },
+    {
+        abr:"VY",
+        val: "très"
+    },
+    {
+        abr:"WX",
+        val: "Condition météo"
+    },
+    {
+        abr:"XYL",
+        val: "femme, épouse(ex youg lady)"
+    },
+    {
+        abr:"YL",
+        val: "Jeune femme (youg lady)"
+    },
+    {
+        abr:"73",
+        val: "Meilleures salutation"
+    },
+    {
+        abr:"5NN",
+        val: "RST"
+    },
+    {
+        abr:"",
+        val: ""
+    },
+
+
+]
